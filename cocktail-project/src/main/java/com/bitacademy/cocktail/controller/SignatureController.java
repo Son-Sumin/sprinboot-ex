a9a0f326a5668a51f92db3f01b6888a08e6ecff7
Son-Sumin <goldkitty007@gmail.com>
2023-02-02 22:00:54 +0900

    cocktail project

--- cocktail-project/src/main/java/com/bitacademy/cocktail/controller/SignatureController.java
+++ cocktail-project/src/main/java/com/bitacademy/cocktail/controller/SignatureController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.bitacademy.cocktail.domain.Signature;
@@ -63,10 +64,17 @@ public class SignatureController {
 		return "signature";
 	}
 	
-	// 시그니처 게시글 수정
-	@GetMapping("/signature/modify")
-	public String modify(Long no, Model model) {;
+	// 시그니처 게시글 수정폼
+	@GetMapping("/signature/modify/{no}")
+	public String modify(@PathVariable Long no, Model model) {;
 		model.addAttribute("signature", signatureService.findSigContents(no));
+		return "signatureModify";
+	}
+	
+	// 시그니처 게시글 수정
+	@PostMapping("/signature/modify/{no}")
+	public String modify(@PathVariable Long no, Signature signature) {;
+		//signature.addAttribute("signature", signatureService.findSigContents(no));
 		return "redirect:/signature";
 	}
 	
