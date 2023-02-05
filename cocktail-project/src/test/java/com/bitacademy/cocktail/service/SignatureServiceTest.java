079fd26ef2c18516bb6174e671ae6ff1e25f60a2
Son-Sumin <goldkitty007@gmail.com>
2023-02-05 15:43:15 +0900

    service test 진행

--- cocktail-project/src/test/java/com/bitacademy/cocktail/service/SignatureServiceTest.java
+++ cocktail-project/src/test/java/com/bitacademy/cocktail/service/SignatureServiceTest.java
@@ -2,7 +2,10 @@ package com.bitacademy.cocktail.service;
 
 import java.time.LocalDateTime;
 
+import javax.transaction.Transactional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.bitacademy.cocktail.domain.Signature;
@@ -10,6 +13,9 @@ import com.bitacademy.cocktail.domain.Signature;
 @SpringBootTest
 public class SignatureServiceTest {
 
+	@Autowired
+	SignatureService signatureService;
+	
 	@Test
 	void testAdd() {
 		Signature signature = Signature.builder()
@@ -20,15 +26,33 @@ public class SignatureServiceTest {
 				.recipeContents("빙글빙글")
 				.type("alcohol")
 				.build();
+		
+		signatureService.add(signature);
 	}
 	
 	@Test
 	void testModify() {
+		/* 전체수정도 하지만 부분수정할 경우 고려하여 아래 방법 실시
+		Signature signature = new Signature();
 		
+		signature.setNo(1);
+		signature.setNickname("수정");
+		signature.setRegDate(LocalDateTime.now());
+		signature.setCocktailName("수정");
+		signature.setCocktailContents("수정");
+		signature.setRecipeContents("수정");
+		signature.setType("alcohol");
+		*/
+		
+		Signature signature = signatureService.findSigView(1L);
+		signature.setCocktailContents("이것만 수정");
+		
+		signatureService.modify(signature);
 	}
 	
 	@Test
+	@Transactional  // Rollback 처리
 	void testDelete() {
-		
+		signatureService.delete(1L);
 	}
 }
