10b81c58383119cd675d7fd58d1b350c1b165889
Son-Sumin <goldkitty007@gmail.com>
2023-03-06 21:34:26 +0900

    jwt...

--- cocktail-project/src/main/java/com/bitacademy/cocktail/config/SecurityConfig.java
+++ cocktail-project/src/main/java/com/bitacademy/cocktail/config/SecurityConfig.java
@@ -1,8 +1,5 @@
 package com.bitacademy.cocktail.config;
 
-
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -25,11 +22,13 @@ public class SecurityConfig {
 
 	private final JwtTokenProvider jwtTokenProvider;
 	
+	// 비밀번호 암호화
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 	
+	// authenticationManager를 Bean 등록
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
@@ -38,6 +37,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
+			// http.httpBasic().disable(); // 일반적인 루트가 아닌 다른 방식으로 요청시 거절, header에 id, pw가 아닌 token(jwt)을 달고 간다. 그래서 basic이 아닌 bearer를 사용한다.
 			.httpBasic().disable()
 			.csrf().disable()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -50,6 +50,7 @@ public class SecurityConfig {
 			.anyRequest().permitAll()
 			.and()
 			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+			// JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다
 		
 		return http.build();
 	}
--- cocktail-project/src/main/java/com/bitacademy/cocktail/jwt/JwtTokenProvider.java
+++ cocktail-project/src/main/java/com/bitacademy/cocktail/jwt/JwtTokenProvider.java
@@ -25,40 +25,48 @@ public class JwtTokenProvider {
 
 private String secretKey = "cocktailproject";
 	
+	// 토큰 유효시간 60분
 	private long tokenValidTime = 60 * 60 * 1000L;
 	
 	private final UserDetailsService userDetailsService;
 	
+	// 객체 초기화, secretKey를 Base64로 인코딩한다.
 	protected void init() {
 		secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
 	}
 	
+	// JWT 토큰 생성
 	public String createToken(String userPk, Role roles) {
-		Claims claims = Jwts.claims().setSubject(userPk);
-		claims.put("roles", roles);
+		Claims claims = Jwts.claims().setSubject(userPk);   // JWT payload 에 저장되는 정보단위, 보통 여기서 user를 식별하는 값을 넣는다.
+		claims.put("roles", roles);  // 정보는 key / value 쌍으로 저장된다.
 		Date now = new Date();
 		return Jwts.builder()
-				.setClaims(claims)
-				.setIssuedAt(now)
-				.setExpiration(new Date(now.getTime() + tokenValidTime))
-				.signWith(SignatureAlgorithm.HS256, secretKey)
+				.setClaims(claims)  // 정보 저장
+				.setIssuedAt(now)   // 토큰 발행 시간 정보
+				.setExpiration(new Date(now.getTime() + tokenValidTime)) // set Expire Time
+				.signWith(SignatureAlgorithm.HS256, secretKey)  // 사용할 암호화 알고리즘과
+				// signature 에 들어갈 secret값 세팅
 				.compact();
 		
 	}
 	
+	// JWT 토큰에서 인증 정보 조회
 	public Authentication getAuthentication(String token) {
 		UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(token));
 		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
 	}
 	
+	// 토큰에서 회원 정보 추출
 	public String getUserPk(String token) {
 		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
 	}
 	
+	// Request의 Header에서 token 값을 가져옵니다. "Authorization" : "TOKEN값'
 	public String resolveToken(HttpServletRequest request) {
 		return request.getHeader("X-TOKEN");
 	}
 	
+	// 토큰의 유효성 + 만료일자 확인
 	public boolean validateToken(String jwtToken) {
 		try {
 			Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
