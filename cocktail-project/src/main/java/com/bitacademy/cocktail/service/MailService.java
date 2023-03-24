26e21b957854da2bb4a5c5f75b1acc330b6f06af
Son-Sumin <goldkitty007@gmail.com>
2023-03-24 18:51:03 +0900

    mail 주석

--- cocktail-project/src/main/java/com/bitacademy/cocktail/service/MailService.java
+++ cocktail-project/src/main/java/com/bitacademy/cocktail/service/MailService.java
@@ -1,60 +1,60 @@
-package com.bitacademy.cocktail.service;
-
-import java.util.Random;
-
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.bitacademy.cocktail.domain.Mail;
-
-import lombok.AllArgsConstructor;
-
-@Service
-@Transactional
-@AllArgsConstructor
-public class MailService {
-
-    private JavaMailSender javaMailSender;
-    private static final String FROM_ADDRESS = "goldkitty007@gmail.com";
-    static StringBuffer key = new StringBuffer();
-
-    public void mailSend(Mail mail){
-        try {
-            MailHandler mailHandler = new MailHandler(javaMailSender);
-            
-            // 받는 사람
-            mailHandler.setTo(mail.getAddress());
-            // 제목
-            mailHandler.setSubject("인증메일입니다.");
-            Random rnd = new Random();
-
-            for (int i = 0; i < 6; i++) {
-                int index = rnd.nextInt(3);
-                switch (index) {
-                    case 0:
-                        key.append(((int) (rnd.nextInt(26)) + 97));
-                        break;
-                    case 1:
-                        key.append(((int) (rnd.nextInt(26)) + 65));
-                        break;
-                    case 2:
-                        key.append((rnd.nextInt(10)));
-                        break;
-                }
-            }
-            String htmlContent = "<p> 인증번호:" + key.toString() +"<p>";
-            mailHandler.setText(htmlContent, true);
-            //mailHandler.setAttach(mail.getFile().getOriginalFilename(), mail.getFile());
-            //mailHandler.setInline("sample-img", mail.getFile());
-            mailHandler.send();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    public boolean answer(String ans){
-        return ans.equals(key.toString());
-    }
-}
+//package com.bitacademy.cocktail.service;
+//
+//import java.util.Random;
+//
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import com.bitacademy.cocktail.domain.Mail;
+//
+//import lombok.AllArgsConstructor;
+//
+//@Service
+//@Transactional
+//@AllArgsConstructor
+//public class MailService {
+//
+//    private JavaMailSender javaMailSender;
+//    private static final String FROM_ADDRESS = "goldkitty007@gmail.com";
+//    static StringBuffer key = new StringBuffer();
+//
+//    public void mailSend(Mail mail){
+//        try {
+//            MailHandler mailHandler = new MailHandler(javaMailSender);
+//            
+//            // 받는 사람
+//            mailHandler.setTo(mail.getAddress());
+//            // 제목
+//            mailHandler.setSubject("인증메일입니다.");
+//            Random rnd = new Random();
+//
+//            for (int i = 0; i < 6; i++) {
+//                int index = rnd.nextInt(3);
+//                switch (index) {
+//                    case 0:
+//                        key.append(((int) (rnd.nextInt(26)) + 97));
+//                        break;
+//                    case 1:
+//                        key.append(((int) (rnd.nextInt(26)) + 65));
+//                        break;
+//                    case 2:
+//                        key.append((rnd.nextInt(10)));
+//                        break;
+//                }
+//            }
+//            String htmlContent = "<p> 인증번호:" + key.toString() +"<p>";
+//            mailHandler.setText(htmlContent, true);
+//            //mailHandler.setAttach(mail.getFile().getOriginalFilename(), mail.getFile());
+//            //mailHandler.setInline("sample-img", mail.getFile());
+//            mailHandler.send();
+//        }
+//        catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public boolean answer(String ans){
+//        return ans.equals(key.toString());
+//    }
+//}
